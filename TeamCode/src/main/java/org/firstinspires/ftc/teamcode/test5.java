f4e2987252775328efabc9b9671cb2d668c8f86b
jiho-0 <chuschus0205@gmail.com>
2026-07-28 22:52:28 +0900

    로봇 앞뒤좌우,회전 움직임

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test5.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "test5")
+public class test5 extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        Mecanum_Driving drive = new Mecanum_Driving(hardwareMap);
+        waitForStart();
+
+        if (opModeIsActive()) {
+            // Pre-run
+            while (opModeIsActive()) {
+                double x = gamepad1.left_stick_x;
+                double y = -gamepad1.left_stick_y;
+                double turn = gamepad1.right_stick_x;
+                // OpMode loop
+
+            }
+        }
+    }
+}
