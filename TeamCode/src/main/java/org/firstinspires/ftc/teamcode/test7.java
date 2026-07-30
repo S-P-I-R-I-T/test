f8c9694d1fb4841d976640548fe579d115b88790
jiho-0 <chuschus0205@gmail.com>
2026-07-30 14:10:21 +0900

    인테이크,기본 움직임

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test7.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test7.java
@@ -3,8 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "test6") //인테이크
-public class test6 extends LinearOpMode {
+@TeleOp(name = "test7")
+public class test7 extends LinearOpMode {
+    Mecanum_Driving drive;
     ActionClass action;
 
     @Override
@@ -14,17 +15,22 @@ public class test6 extends LinearOpMode {
         waitForStart();
 
         if (opModeIsActive()) {
+            // Pre-run
             while (opModeIsActive()) {
                 // OpMode loop
+                drive.drive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+
                 initIntake();
+
                 telemetry.update();
+
             }
         }
     }
     public void Init() {
+        drive = new Mecanum_Driving(hardwareMap);
         action = new ActionClass(hardwareMap);
     }
-
     public void initIntake() {
         if (gamepad1.a) {
             action.Intake_On();
