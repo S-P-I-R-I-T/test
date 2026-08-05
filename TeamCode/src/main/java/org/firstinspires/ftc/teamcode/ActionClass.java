d0b012682b77fcf5c8f323f0b7f23235e499a564
jochaehoon <jochaehun734@gmai.com>
2026-08-05 16:33:52 +0900

    ActionClass+wheel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ActionClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ActionClass.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -15,6 +16,16 @@ public class ActionClass {
 
     //intake
     public DcMotorEx intakeMotor;
+
+    //슈퍼 스피드 서보
+    public CRServo wheel1;
+    public CRServo wheel2;
+
+    //이걸 바꿔서 휠 코드 충돌 방지
+    //발사할 때 shoot = true
+    public boolean shoot = false;
+    public boolean intake = false;
+
     public ActionClass(HardwareMap hardwareMap) {
         //pdif
         shootMotor = hardwareMap.get(DcMotorEx.class,"shootMotor");
@@ -25,25 +36,47 @@ public class ActionClass {
 
         //intake
         intakeMotor = hardwareMap.get(DcMotorEx.class,"intakeMotor");
-        intakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         intakeMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        //슈퍼 스피드 서보
+        wheel1 = hardwareMap.get(CRServo.class,"wheel1");
+        wheel2 = hardwareMap.get(CRServo.class,"wheel2");
+        wheel1.setDirection(CRServo.Direction.FORWARD);
+        wheel2.setDirection(CRServo.Direction.REVERSE);
     }
     //pdif
     public void shoot() {
         shootMotor.setVelocity(shooting_velocity);
+        wheel1.setPower(-1);
+        wheel2.setPower(-1);
+
     }
     public void preheat() {
         shootMotor.setVelocity(preheat_velocity);
     }
+    public void stop(){
+        shootMotor.setVelocity(0);
+    }
 
-    //intake
+    //intake & 슈퍼 스피드 서보
     public void Intake_On(){
         intakeMotor.setPower(1);
+        if(shoot == false){
+            wheel1.setPower(1);
+            wheel2.setPower(1);
+        }
     }
     public void Intake_R(){
         intakeMotor.setPower(-1);
+        wheel1.setPower(-1);
+        wheel2.setPower(-1);
     }
     public void Intake_Off(){
         intakeMotor.setPower(0);
+        wheel1.setPower(0);
+        wheel2.setPower(0);
+
     }
+
 }
