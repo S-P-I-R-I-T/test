8317c53347f51e4c59e5440156cbf7f60473cb26
jiho-0 <chuschus0205@gmail.com>
2026-08-05 16:54:57 +0900

    움직임,슈팅,인테이크

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Action.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Action")
+public class Action extends LinearOpMode {
+    ActionClass action;
+
+    @Override
+    public void runOpMode() {
+        Init();
+        waitForStart();
+
+        while (opModeIsActive()) {
+            initAction();
+            telemetry.update();
+        }
+    }
+
+    public void Init() {
+        action = new ActionClass(hardwareMap);
+    }
+
+    public void initAction() {
+        // 슈팅
+        if (gamepad2.right_trigger > 0.1) {
+            action.shoot();
+        } else if (gamepad2.left_trigger > 0.1) {
+            action.preheat();
+        } else {
+            action.shootMotor.setVelocity(0);
+        }
+
+        // 인테이크
+        if (gamepad2.a) {
+            action.Intake_On();
+        } else if (gamepad2.b) {
+            action.Intake_R();
+        } else {
+            action.Intake_Off();
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheel.java
@@ -1,66 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.opencv.core.Mat;
+@TeleOp(name = "Wheel")
+public class Wheel extends LinearOpMode {
+    WheelClass wheel;
 
-public class Wheel {
-    private DcMotor fl, fr, rl, rr;
-    private static final double Base_Speed = 0.7;
-    private static final double Slow_Multiplier = 0.5;
-    public Wheel(HardwareMap hardwareMap) {
-        fl = hardwareMap.get(DcMotor.class,"fl");
-        rl = hardwareMap.get(DcMotor.class,"rl");
-        fr = hardwareMap.get(DcMotor.class,"fr");
-        rr = hardwareMap.get(DcMotor.class,"rr");
+    @Override
+    public void runOpMode() {
+        Init();
+        waitForStart();
 
-        fl.setDirection(DcMotorSimple.Direction.REVERSE);
-        rl.setDirection(DcMotorSimple.Direction.REVERSE);
-        fr.setDirection(DcMotorSimple.Direction.FORWARD);
-        rr.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        while (opModeIsActive()) {
+            driveControl();
+            telemetry.update();
+        }
     }
 
-    public void drive(double axial,double lateral,double yaw,boolean slow) {
-
-        double speed;
-        if (slow) {
-            speed = Base_Speed * Slow_Multiplier;
-        } else {
-            speed = Base_Speed;
-        }
-        double flpower = axial + lateral + yaw;
-        double frpower = axial - lateral - yaw;
-        double rlpower = axial - lateral + yaw;
-        double rrpower = axial + lateral - yaw;
-
-        double max = Math.max(
-                Math.max(Math.abs(flpower), Math.abs(frpower)),
-                Math.max(Math.abs(rlpower), Math.abs(rrpower))
-        );
-        if (max > 1.0) {
-            flpower /= max;
-            frpower /= max;
-            rlpower /= max;
-            rrpower /= max;
-
-        }
-
-        fl.setPower(flpower * speed);
-        fr.setPower(frpower * speed);
-        rl.setPower(rlpower * speed);
-        rr.setPower(rrpower * speed);
+    public void Init() {
+        wheel = new WheelClass(hardwareMap);
     }
-    public void stop() {
-        fl.setPower(0);
-        fr.setPower(0);
-        rl.setPower(0);
-        rr.setPower(0);
+
+    public void driveControl() {
+        double axial = -gamepad1.left_stick_y;
+        double lateral = gamepad1.left_stick_x;
+        double yaw = gamepad1.right_stick_x;
+        boolean slow = gamepad1.right_bumper;
+
+        wheel.drive(axial, lateral, yaw, slow);
     }
 }
